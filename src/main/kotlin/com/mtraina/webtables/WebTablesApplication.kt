edb9565438122e7d80c1e59f3a0d1bf96df314af
mtraina <matteo.traina.mail@gmail.com>
2018-04-08 20:53:47 +0200

    project creation

---
+++ src/main/kotlin/com/mtraina/webtables/WebTablesApplication.kt
@@ -0,0 +1,11 @@
+package com.mtraina.webtables
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebTablesApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebTablesApplication>(*args)
+}
