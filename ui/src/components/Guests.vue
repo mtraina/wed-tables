2e4e50eb5e4562149406ba6ed8a6572c22a04e62
mtraina <matteo.traina.mail@gmail.com>
2018-04-15 21:22:28 +0200

    get guests from ui I

---
+++ ui/src/components/Guests.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <button class=”Search__button” @click="callRestService()">Get guests</button>
+    <h3>{{ response }}</h3>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+
+  data () {
+    return {
+      response: [],
+      errors: []
+    }
+  },
+
+  callRestService () {
+    axios.get(`api/guests`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.response = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+  }
+}
+</script>
--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Guests from '@/components/Guests'
 
 Vue.use(Router)
 
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Guests',
+      component: Guests
+    },
+    {
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
     }
