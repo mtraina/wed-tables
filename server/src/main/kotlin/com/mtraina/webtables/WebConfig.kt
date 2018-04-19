fc8d455080ab27b24b5edcb09f0368886f666743
mtraina <matteo.traina.mail@gmail.com>
2018-04-19 22:12:43 +0200

    CORS server side fix

--- server/src/main/kotlin/com/mtraina/webtables/WebConfig.kt
+++ server/src/main/kotlin/com/mtraina/webtables/WebConfig.kt
@@ -1,23 +1,23 @@
-//package com.mtraina.webtables
-//
-//import org.springframework.context.annotation.Configuration
-//import org.springframework.web.reactive.config.CorsRegistry
-//import org.springframework.web.reactive.config.EnableWebFlux
-//import org.springframework.web.reactive.config.WebFluxConfigurer
-//
-//
-//@Configuration
-//@EnableWebFlux
-//class WebConfig : WebFluxConfigurer {
-//
-//    override fun addCorsMappings(registry: CorsRegistry?) {
-//
-//        registry!!.addMapping("/api/**")
-//                .allowedOrigins("http://localhost:8090")
-//                .allowedMethods("GET")
-////                .allowedHeaders("header1", "header2", "header3")
-////                .exposedHeaders("header1", "header2")
-////                .allowCredentials(true).maxAge(3600)
-//
-//    }
-//}
+package com.mtraina.webtables
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+
+@Configuration
+@EnableWebFlux
+class WebConfig : WebFluxConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:8090")
+                //.allowedMethods("GET")
+//                .allowedHeaders("header1", "header2", "header3")
+//                .exposedHeaders("header1", "header2")
+//                .allowCredentials(true).maxAge(3600)
+
+    }
+}
