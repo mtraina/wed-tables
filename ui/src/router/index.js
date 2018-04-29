0aa4c69b74a58f6850589418bb72419107c722a4
mtraina <matteo.traina.mail@gmail.com>
2018-04-29 10:30:32 +0200

    draggable introduced

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Guests from '@/components/Guests'
+import Tables from '@/components/Tables'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Guests',
       component: Guests
+    },
+    {
+      path: '/tables',
+      name: 'Tables',
+      component: Tables
     },
     {
       path: '/hello',
