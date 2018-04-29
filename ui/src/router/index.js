8ed4ec9da6c1445f6f1fec24de83b8417e289716
mtraina <matteo.traina.mail@gmail.com>
2018-04-29 12:10:29 +0200

    dummy file removed

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Guests from '@/components/Guests'
 import Tables from '@/components/Tables'
 
@@ -17,11 +16,6 @@ export default new Router({
       path: '/tables',
       name: 'Tables',
       component: Tables
-    },
-    {
-      path: '/hello',
-      name: 'HelloWorld',
-      component: HelloWorld
     }
   ]
 })
