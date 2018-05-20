2f618ce1365a510dd91461dd5bb5aad3a6e53e15
mtraina <matteo.traina.mail@gmail.com>
2018-05-20 18:42:05 +0200

    refactoring II

--- ui/src/components/Tables.vue
+++ ui/src/components/Tables.vue
@@ -38,13 +38,7 @@
     <Table tableId="table14" title="14"/>
     <Table tableId="table15" title="15"/>
     <Table tableId="table16" title="16"/>
-
-    <!-- <div  class="list-group col-md-2">
-      <pre>{{listString}}</pre>
-    </div>
-     <div  class="list-group col-md-2">
-      <pre>{{list2String}}</pre>
-    </div> -->
+    
   </div>
 </template>
 
@@ -68,22 +62,6 @@ export default {
   data() {
     return {
       guestList:[],
-      table1:[],
-      table2:[],
-      table3:[],
-      table4:[],
-      table5:[],
-      table6:[],
-      table7:[],
-      table8:[],
-      table9:[],
-      table10:[],
-      table11:[],
-      table12:[],
-      table13:[],
-      table14:[],
-      table15:[],
-      table16:[],
 
       editable:true,
       isDragging: false,
@@ -94,41 +72,9 @@ export default {
       console.log('App mounted!');
 
       if(localStorage.getItem('guestList')){
-        this.guestList = JSON.parse(localStorage.getItem("guestList"));
-        this.table1 = JSON.parse(localStorage.getItem("table1"));
-        this.table2 = JSON.parse(localStorage.getItem("table2"));
-        this.table3 = JSON.parse(localStorage.getItem("table3"));
-        this.table4 = JSON.parse(localStorage.getItem("table4"));
-        this.table5 = JSON.parse(localStorage.getItem("table5"));
-        this.table6 = JSON.parse(localStorage.getItem("table6"));
-        this.table7 = JSON.parse(localStorage.getItem("table7"));
-        this.table8 = JSON.parse(localStorage.getItem("table8"));
-        this.table9 = JSON.parse(localStorage.getItem("table9"));
-        this.table10 = JSON.parse(localStorage.getItem("table10"));
-        this.table11 = JSON.parse(localStorage.getItem("table11"));
-        this.table12 = JSON.parse(localStorage.getItem("table12"));
-        this.table13 = JSON.parse(localStorage.getItem("table13"));
-        this.table14 = JSON.parse(localStorage.getItem("table14"));
-        this.table15 = JSON.parse(localStorage.getItem("table15"));
-        this.table16 = JSON.parse(localStorage.getItem("table16"));
+        this.guestList = JSON.parse(localStorage.getItem("guestList"))
       } else {
         this.guestList = guests.filter(e => e.presence).map((e, index) => { return {name:e.name, order: index+1, fixed: false} });
-        this.table1 = []
-        this.table2 = []
-        this.table3 = []
-        this.table4 = []
-        this.table5 = []
-        this.table6 = []
-        this.table7 = []
-        this.table8 = []
-        this.table9 = []
-        this.table10 = []
-        this.table11 = []
-        this.table12 = []
-        this.table13 = []
-        this.table14 = []
-        this.table15 = []
-        this.table16 = []
       }
     },
   methods:{
@@ -144,23 +90,6 @@ export default {
       console.log('This is before the write call');
 
       localStorage.setItem("guestList", JSON.stringify(this.guestList));
-      localStorage.setItem("table1", JSON.stringify(this.table1));
-      localStorage.setItem("table2", JSON.stringify(this.table2));
-      localStorage.setItem("table3", JSON.stringify(this.table3));
-      localStorage.setItem("table4", JSON.stringify(this.table4));
-      localStorage.setItem("table5", JSON.stringify(this.table5));
-      localStorage.setItem("table6", JSON.stringify(this.table6));
-      localStorage.setItem("table7", JSON.stringify(this.table7));
-      localStorage.setItem("table8", JSON.stringify(this.table8));
-      localStorage.setItem("table9", JSON.stringify(this.table9));
-      localStorage.setItem("table10", JSON.stringify(this.table10));
-      localStorage.setItem("table11", JSON.stringify(this.table11));
-      localStorage.setItem("table12", JSON.stringify(this.table12));
-      localStorage.setItem("table13", JSON.stringify(this.table13));
-      localStorage.setItem("table14", JSON.stringify(this.table14));
-      localStorage.setItem("table15", JSON.stringify(this.table15));
-      localStorage.setItem("table16", JSON.stringify(this.table16));
-
       EventBus.$emit('save');
 
       console.log('This is after the write call');
